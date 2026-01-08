ccd1a07346edef5ee7052e3a735589a452fb856b
david0-012 <davidcasallas1202@gmail.com>
2026-01-08 11:42:21 -0500

    Configuración de checkpointer dinámico con FastAPI y Postgres

--- src/api/main.py
+++ src/api/main.py
@@ -1,13 +1,15 @@
 from dotenv import load_dotenv
 load_dotenv()
-from fastapi import FastAPI
+
 from pydantic import BaseModel
-from agents.support.agent import agent
+
+from fastapi import FastAPI
+from agents.support.agent import make_graph
 from langchain_core.messages import HumanMessage
 from fastapi.responses import StreamingResponse
+from api.db import lifespan, CheckpointerDep
 
-app = FastAPI()
-
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 def read_root():
@@ -17,21 +19,27 @@ class Message(BaseModel):
     message: str
 
 @app.post("/chat/{chat_id}")
-async def chat(chat_id: str, item: Message):
+async def chat(chat_id: str, item: Message, checkpointer: CheckpointerDep):
+    # CRUD add message
     config = {
         "configurable": {
-            "thread_id": chat_id
+            "thread_id": chat_id,
         }
     }
     human_message = HumanMessage(content=item.message)
-    response = await agent.ainvoke({"messages": [human_message]})
+    agent = make_graph(config={"checkpointer": checkpointer})
+    state = {"messages": [human_message]}
+    response = agent.invoke(state, config)
     last_message = response["messages"][-1]
-    return last_message.text
+    # CRUD add message
+    return response["messages"]
+
 
 @app.post("/chat/{chat_id}/stream")
-async def stream_chat(chat_id: str, message: Message):
+async def stream_chat(chat_id: str, message: Message, checkpointer: CheckpointerDep):
     human_message = HumanMessage(content=message.message)
     async def generate_response():
+        agent = make_graph(config={"checkpointer": checkpointer})
         for message_chunk, metadata in agent.stream({"messages": [human_message]}, stream_mode="messages"):
             if message_chunk.content:
                 yield f"data: {message_chunk.content}\n\n"
